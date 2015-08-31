4e1453474ee8833faf3ecda8bf88193b46670a8d
Dylan Corado <nos_196@hotmail.com>
2015-08-31 15:49:24 -0700

    completo

---
+++ Proyecto/Laboratorio/frmBuscarExamen.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Laboratorio
+{
+    partial class frmBuscarExamen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmBuscarExamen));
+            this.grdTipoExamen = new System.Windows.Forms.DataGridView();
+            this.Codigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblCodigoPaciente = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnAtras = new System.Windows.Forms.Button();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.grdTipoExamen)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grdTipoExamen
+            // 
+            this.grdTipoExamen.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdTipoExamen.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Codigo,
+            this.Nombre});
+            this.grdTipoExamen.Location = new System.Drawing.Point(164, 61);
+            this.grdTipoExamen.Name = "grdTipoExamen";
+            this.grdTipoExamen.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grdTipoExamen.Size = new System.Drawing.Size(482, 261);
+            this.grdTipoExamen.TabIndex = 46;
+            this.grdTipoExamen.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grdTipoExamen_CellContentClick);
+            // 
+            // Codigo
+            // 
+            this.Codigo.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.Codigo.HeaderText = "Codigo";
+            this.Codigo.Name = "Codigo";
+            this.Codigo.ReadOnly = true;
+            this.Codigo.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Codigo.Width = 65;
+            // 
+            // Nombre
+            // 
+            this.Nombre.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            this.Nombre.ReadOnly = true;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(280, 24);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(229, 20);
+            this.txtNombre.TabIndex = 45;
+            this.txtNombre.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtNombre_KeyUp);
+            // 
+            // lblCodigoPaciente
+            // 
+            this.lblCodigoPaciente.AutoSize = true;
+            this.lblCodigoPaciente.BackColor = System.Drawing.Color.Transparent;
+            this.lblCodigoPaciente.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodigoPaciente.Location = new System.Drawing.Point(205, 25);
+            this.lblCodigoPaciente.Name = "lblCodigoPaciente";
+            this.lblCodigoPaciente.Size = new System.Drawing.Size(64, 19);
+            this.lblCodigoPaciente.TabIndex = 44;
+            this.lblCodigoPaciente.Text = "Nombre:";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btnAtras);
+            this.panel1.Controls.Add(this.btnAceptar);
+            this.panel1.Controls.Add(this.btnCancelar);
+            this.panel1.Location = new System.Drawing.Point(12, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(133, 272);
+            this.panel1.TabIndex = 43;
+            // 
+            // btnAtras
+            // 
+            this.btnAtras.Font = new System.Drawing.Font("Calibri", 9.75F);
+            this.btnAtras.Image = ((System.Drawing.Image)(resources.GetObject("btnAtras.Image")));
+            this.btnAtras.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAtras.Location = new System.Drawing.Point(10, 174);
+            this.btnAtras.Name = "btnAtras";
+            this.btnAtras.Size = new System.Drawing.Size(110, 50);
+            this.btnAtras.TabIndex = 7;
+            this.btnAtras.Text = "Atras";
+            this.btnAtras.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnAtras.UseVisualStyleBackColor = true;
+            this.btnAtras.Click += new System.EventHandler(this.btnAtras_Click);
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Enabled = false;
+            this.btnAceptar.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAceptar.Image = ((System.Drawing.Image)(resources.GetObject("btnAceptar.Image")));
+            this.btnAceptar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAceptar.Location = new System.Drawing.Point(12, 39);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(110, 50);
+            this.btnAceptar.TabIndex = 5;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Font = new System.Drawing.Font("Calibri", 9.75F);
+            this.btnCancelar.Image = ((System.Drawing.Image)(resources.GetObject("btnCancelar.Image")));
+            this.btnCancelar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCancelar.Location = new System.Drawing.Point(12, 107);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(110, 50);
+            this.btnCancelar.TabIndex = 6;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // frmBuscarExamen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(658, 334);
+            this.ControlBox = false;
+            this.Controls.Add(this.grdTipoExamen);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblCodigoPaciente);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmBuscarExamen";
+            this.Text = "Buscar Examen";
+            ((System.ComponentModel.ISupportInitialize)(this.grdTipoExamen)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grdTipoExamen;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Codigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblCodigoPaciente;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnAtras;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCancelar;
+
+    }
+}
---
+++ Proyecto/Laboratorio/frmBuscarExamen.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Laboratorio
+{
+    public partial class frmBuscarExamen : Form
+    {
+        string sCodigoTabla;
+        string sNombreTabla;
+        public frmBuscarExamen()
+        {
+            InitializeComponent();
+            funActualizar();
+        }
+
+        void funActualizar()
+        {
+            string sCodigo;
+            string sNombre;
+            int iContador = 0;
+            grdTipoExamen.Rows.Clear();
+
+            try
+            {
+                MySqlCommand mComando = new MySqlCommand(String.Format(
+                "SELECT ncodtipo, cdesctipoexamen FROM MaTIPOEXAMEN "), clasConexion.funConexion());
+                MySqlDataReader mReader = mComando.ExecuteReader();
+
+                    while (mReader.Read())
+                    {
+                        sCodigo = mReader.GetString(0);
+                        sNombre = mReader.GetString(1);
+                        grdTipoExamen.Rows.Insert(iContador, sCodigo, sNombre);
+                        sCodigo = "";
+                        sNombre = "";
+                        iContador++;
+                    } 
+            }
+            catch
+            {
+                MessageBox.Show("Se produjo un error", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+        }
+
+
+        private void btnAtras_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtNombre_KeyUp(object sender, KeyEventArgs e)
+        {
+            string sCodigo;
+            string sNombre;
+            int iContador = 0;
+            grdTipoExamen.Rows.Clear();
+
+            try
+            {
+                MySqlCommand mComando = new MySqlCommand(String.Format(
+                "SELECT ncodtipo, cdesctipoexamen FROM MaTIPOEXAMEN WHERE cdesctipoexamen LIKE '{0}%' ",txtNombre.Text), clasConexion.funConexion());
+                MySqlDataReader mReader = mComando.ExecuteReader();
+
+                while (mReader.Read())
+                {
+                    sCodigo = mReader.GetString(0);
+                    sNombre = mReader.GetString(1);
+                    grdTipoExamen.Rows.Insert(iContador, sCodigo, sNombre);
+                    sCodigo = "";
+                    sNombre = "";
+                    iContador++;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Se produjo un error", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            txtNombre.Text = "";
+            btnCancelar.Enabled = btnAceptar.Enabled = false;
+            funActualizar();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            frmReporteTipoExamen ver = new frmReporteTipoExamen();
+            ver.sFormTipoExamen = sCodigoTabla;
+            ver.sFormNombreExamen = sNombreTabla;
+            ver.txtTipoExamen.Text = sCodigoTabla + ". " + sNombreTabla;
+            ver.Show();
+            this.Close();
+        }
+
+        private void grdTipoExamen_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            btnAceptar.Enabled = true;
+            DataGridViewRow fila = grdTipoExamen.CurrentRow;
+            sCodigoTabla = Convert.ToString(fila.Cells[0].Value);
+            sNombreTabla = Convert.ToString(fila.Cells[1].Value);
+        }
+    }
+}
